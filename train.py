d3dbee947157a931c605c21473faa1de86b1f456
hjkim <hjkim@hjkim-MacBookPro.local>
2023-12-14 08:45:51 +0900

    add_traincode_231214

--- train.py
+++ train.py
@@ -1,18 +1,20 @@
 #%% Import Packages
 import os
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms, datasets
 
-#%% Training Parameters
+# Training Parameters
 lr = 1e-3
 BATCH_SIZE = 4
 NUM_EPOCHS = 100
 
-data_dir = './datasets'
+data_dir = './dataset'
 ckpt_dir = './checkpoint'
 log_dir = './log'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -24,7 +26,7 @@ class UNet(nn.Module):
 
         def CBR2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True):    # Conv + BatchNorm + ReLU
             layers = []
-            layers += [[nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)]]
+            layers += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)]
             layers += [nn.BatchNorm2d(num_features=out_channels)]
             layers += [nn.ReLU()]
 
@@ -81,7 +83,7 @@ class UNet(nn.Module):
         self.dec1_1 = CBR2d(in_channels=64, out_channels=64)
 
         # number of class
-        self.fc = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1, stride=1, padding=1, bias=True)
+        self.fc = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0, bias=True)
         
     def forward(self, x):
         # Encoder - Contracting Path
@@ -106,20 +108,20 @@ class UNet(nn.Module):
         # Decoder - Expanding Path
         dec5_1 = self.dec5_1(enc5_1)
 
-        unpool4 = self.unpool3(dec5_1)
+        unpool4 = self.unpool4(dec5_1)
         cat4 = torch.cat((unpool4, enc4_2), dim=1)      # dim=0 > batch, dim=1 > channel, dim=2 > height, dim3 > width
         dec4_2 = self.dec4_2(cat4)
         dec4_1 = self.dec4_1(dec4_2)
 
-        unpool3 = self.unpool2(dec4_1)
+        unpool3 = self.unpool3(dec4_1)
         cat3 = torch.cat((unpool3, enc3_2), dim=1)
         dec3_2 = self.dec3_2(cat3)
         dec3_1 = self.dec3_1(dec3_2)
 
-        unpool2 = self.unpool1(dec3_1)
+        unpool2 = self.unpool2(dec3_1)
         cat2 = torch.cat((unpool2, enc2_2), dim=1)
         dec2_2 = self.dec2_2(cat2)
-        dec2_1 = self.dec2_2(dec2_2)
+        dec2_1 = self.dec2_1(dec2_2)
 
         unpool1 = self.unpool1(dec2_1)
         cat1 = torch.cat((unpool1, enc1_2), dim=1)
@@ -129,5 +131,234 @@ class UNet(nn.Module):
         x = self.fc(dec1_1)
 
         return x
-        
 
+class ImageDataset(torch.utils.data.Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data_dir = data_dir
+        self.transform = transform
+
+        lst_data = os.listdir(self.data_dir)
+
+        lst_label = [f for f in lst_data if f.startswith('label')]
+        lst_input = [f for f in lst_data if f.startswith('input')]
+
+        lst_label.sort()
+        lst_input.sort()
+
+        self.lst_label = lst_label
+        self.lst_input = lst_input
+
+    def __len__(self):
+        return len(self.lst_label)
+    
+    def __getitem__(self, idx):
+        label = np.load(os.path.join(self.data_dir, self.lst_label[idx]))
+        input = np.load(os.path.join(self.data_dir, self.lst_input[idx]))
+
+        label = label / 255.0
+        input = input / 255.0
+
+        if label.ndim == 2:
+            label = label[:, :, np.newaxis]
+        if input.ndim == 2:
+            input = input[:, :, np.newaxis]
+        
+        data = {'input': input, 'label': label}
+        
+        if self.transform:
+            data = self.transform(data)
+
+        return data
+
+class ToTensor(object):
+    def __call__(self, data):
+        label, input = data['input'], data['label']
+        # numpy image : (H, W, CH), torch image: (CH, H, W)
+        label = label.transpose((2, 0, 1)).astype(np.float32)
+        input = input.transpose((2, 0, 1)).astype(np.float32)
+
+        data = {'label': torch.from_numpy(label), 'input': torch.from_numpy(input)}
+
+        return data
+
+
+class Normalization(object):
+    def __init__(self, mean=0.5, std=0.5):
+        self.mean = mean
+        self.std = std
+    
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+
+        input = (input - self.mean) / self.std  # label 은 0, 1 로 되어있으므로 Normalize 불필요
+        data = {'label':label, 'input': input}
+        
+        return data
+
+class RandomFlip(object):
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+        if np.random.rand() > 0.5:
+            label = np.fliplr(label)
+            input = np.fliplr(input)
+
+        if np.random.rand() > 0.5:
+            label = np.flipud(label)
+            input = np.flipud(input)
+
+        data = {'label': label, 'input':input}
+        
+        return data
+
+
+# Transform
+transform = transforms.Compose([
+    Normalization(mean=0.5, std=0.5), RandomFlip(), ToTensor()
+])
+
+dataset_train = ImageDataset(data_dir=os.path.join(data_dir, 'train'), transform=transform)
+loader_train = DataLoader(dataset_train, batch_size=BATCH_SIZE, shuffle=True)
+
+dataset_val = ImageDataset(data_dir=os.path.join(data_dir, 'val'), transform=transform)
+loader_val = DataLoader(dataset_val, batch_size=BATCH_SIZE, shuffle=False)
+
+# Network
+net = UNet().to(device)
+# Loss function
+fn_loss = nn.BCEWithLogitsLoss().to(device)
+# Optimizer
+optim = torch.optim.Adam(net.parameters(), lr=lr)
+# etc
+num_data_train = len(dataset_train)
+num_data_val = len(dataset_val)
+num_batch_train = np.ceil(num_data_train / BATCH_SIZE)
+num_batch_val = np.ceil(num_data_val / BATCH_SIZE)
+# etc function
+fn_tonumpy = lambda x: x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)
+fn_denorm = lambda x, mean, std: (x*std) + mean
+fn_class = lambda x: 1.0 * (x>0.5)
+# Tensorboard
+writer_train = SummaryWriter(log_dir=os.path.join(log_dir, 'train'))
+writer_val = SummaryWriter(log_dir=os.path.join(log_dir, 'val'))
+
+# Network Save
+def save(ckpt_dir, net, optim, epoch):
+    if not os.path.exists(ckpt_dir):
+        os.makedirs(ckpt_dir)
+
+    torch.save({'net': net.state_dict(), 'optim': optim.state_dict()}, f"./{ckpt_dir}/model_epoch{epoch}.pth")
+    
+# Network Load
+def load(ckpt_dir, net, optim):
+    if not os.path.exists(ckpt_dir):
+        epoch = 0
+        return net, optim, epoch
+    
+    ckpt_lst = os.listdir(ckpt_dir)
+    ckpt_lst.sort(key=lambda f: int(''.join(filter(str.indigit, f))))
+
+    dict_model = torch.load(f"{ckpt_dir}/{ckpt_lst[-1]}")
+    
+    net.load_state_dict(dict_model['net'])
+    optim.load_state_dict(dict_model['optim'])
+    epoch = int(ckpt_lst[-1].split('epoch')[1].split('.pth')[0])
+
+    return net, optim, epoch
+
+# Network Training
+ST_EPOCH = 0
+net, optim, ST_EPOCH = load(ckpt_dir=ckpt_dir, net=net, optim=optim)
+
+for epoch in range(ST_EPOCH+1, NUM_EPOCHS+1):
+    net.train()
+    loss_arr = []
+
+    for batch_idx, data in enumerate(loader_train, 1):
+        # forward pass
+        label = data['label'].to(device)
+        input = data['input'].to(device)
+        output = net(input)
+        # backward pass
+        optim.zero_grad()
+        loss = fn_loss(output, label)
+        loss.backward()
+        optim.step()
+
+        # Loss
+        loss_arr += [loss.item()]
+        print(f'TRAIN: EPOCH [{epoch} / {NUM_EPOCHS}] | BATCH [{batch_idx} / {num_batch_train}] | LOSS [{loss}]')
+
+        # To numpy
+        label = fn_tonumpy(label)
+        input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
+        output = fn_tonumpy(fn_class(output))
+        # Tensorboard
+        writer_train.add_image('label', label, num_batch_train * (epoch - 1) + batch_idx, dataformats='NHWC')
+        writer_train.add_image('input', input, num_batch_train * (epoch - 1) + batch_idx, dataformats='NHWC')
+        writer_train.add_image('output', output, num_batch_train * (epoch - 1) + batch_idx, dataformats='NHWC')
+
+    writer_train.add_scalar('loss', np.mean(loss_arr), epoch)
+
+    with torch.no_grad():
+        net.eval()
+        loss_arr = []
+
+        for batch_idx, data in enumerate(loader_val, 1):
+            # forward pass
+            label = data['label'].to(device)
+            input = data['input'].to(device)
+            output = net(input)
+            # Loss 
+            loss = fn_loss(output, label) 
+            loss_arr += [loss.item()]
+            # To numpy
+            label = fn_tonumpy(label)
+            input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
+            output = fn_tonumpy(fn_class(output))
+
+            print(f"VALID: EPOCH [{epoch} / {NUM_EPOCHS}] | BATCH [{batch_idx} / {num_batch_val}] | LOSS [{loss}]")
+
+            # Tensorboard
+            writer_val.add_image('label', label, num_batch_val * (epoch-1) + batch_idx, dataformats='NHWC')
+            writer_val.add_image('input', input, num_batch_val * (epoch-1) + batch_idx, dataformats='NHWC')
+            writer_val.add_image('output', output, num_batch_val * (epoch-1) + batch_idx, dataformats='NHWC')
+    writer_val.add_scalar('loss', np.mean(loss_arr), epoch)
+    
+    if epoch % 10 == 0:
+        save(ckpt_dir=ckpt_dir, net=net, optim=optim, epoch=epoch)
+
+
+
+# Tensorboard close
+writer_train.close()
+writer_val.close()
+
+
+
+# #%% test
+# transform = transforms.Compose([
+#     Normalization(mean=0.5, std=0.5),
+#     RandomFlip(), 
+#     ToTensor()
+# ])
+
+# datatset_train = ImageDataset(data_dir='./dataset/train', transform=transform)
+
+# #%% check sample 
+# data = datatset_train.__getitem__(0)
+# input = data['input']
+# label = data['label']
+
+
+# print('SHAPE')
+# print(f"Input: {input.shape}, Label: {label.shape}")
+
+# print('tpye')
+# print(f"Input: {input.type()}, Label: {label.type()}")
+
+
+# plt.subplot(121)
+# plt.imshow(input.squeeze())
+
+# plt.subplot(122)
+# plt.imshow(label.squeeze())
